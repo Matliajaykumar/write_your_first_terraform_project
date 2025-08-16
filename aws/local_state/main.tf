ec12fa37e3e2dfc2cfb3fa55fb3487141c015edb
Matliajaykumar <ajaykumar.matli888@gmail.com>
2025-08-17 04:08:55 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-002acb8a1137fbc51"
   instance_type = "t3.micro"
 
   tags = {
